775fc9761b511c99798d9f7122b5fd53874a0b02
draymond63 <41529410+draymond63@users.noreply.github.com>
2021-03-11 15:37:16 -0500

    Revert "Testimonial section (#13)"
    This reverts commit 2e21d37413fca84a562dc5a8ea66679d1f55fc4e.

--- src/components/homePage/Speakers.vue
+++ src/components/homePage/Speakers.vue
@@ -1,26 +1,13 @@
 <template>
 <div id="speakers">
 	<h2>CUTC 2020</h2>
-	<div id="TABS">
-		<p id="tab" @click="currentPage = 0"> Speakers </p>
-		<p id="tab" @click="currentPage = 1"> Events </p>
-	</div>
-	<div id="content">
-		<p v-if="currentPage == 0"> speaker info </p>
-		<p v-else> event info </p>
-	</div>
+	<!-- TAB LAYOUT -->
 </div>
 </template>
-
 
 <script>
 export default {
 	name: 'Speakers',
-	data () {
-		return {
-		currentPage: 0,
-		}
-	}
 }
 </script>
 
@@ -29,22 +16,4 @@ export default {
 	margin: 1em;
 	background: var(--nav-color);
 }
-#TABS {
-	margin: 0 auto;
-	display: flex;
-}
-#content {
-	color: white;
-	padding: 40px;
-	display: flex;
-}
-#tab {
-	overflow: hidden;
-	display: flex;
-	padding: 40px;
-	cursor: pointer;
-	margin: 0 auto;
-	text-align: center;
-	float: center;
-}
- </style>
+</style>
